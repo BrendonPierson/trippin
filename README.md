a1a1a758d3be409fa194c3c7bc267be68ab741ff
BrendonPierson <brendonpierson@gmail.com>
2015-08-19 14:04:33 -0500

    added firebase data connection

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# trippin
+# Trippin
---
+++ javascripts/load-data.js
@@ -0,0 +1,13 @@
+define(function(require){
+  var firebase = require('firebase');
+
+  var ref = new Firebase("https://trippin-nss-app.firebaseio.com/");
+
+  // listen for any changes on the "trips" key
+  ref.child('trips').on('value', function(snapshot){
+    var trips = snapshot.val();
+    
+    console.log("trips", trips);
+  });
+
+});
